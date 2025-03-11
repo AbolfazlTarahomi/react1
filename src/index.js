4005eb3f892d0f7651da55c100f0581ecf3a2d20
Abolfazl <abtarahomii@gmail.com>
2025-03-11 23:25:36 +0330

    اضافه کردن تغییرات در App.js و index.js

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ import Home from "./Home";
 import Header from "./Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
 root.render(
   // <React.StrictMode>
   //   <App />
+
   <Home />
+ 
 
   // </React.StrictMode>
 );
